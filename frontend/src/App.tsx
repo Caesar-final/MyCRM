1a26ecac9bd698f79a920916d1ce56d1f86fa5e0
Cesa Andrade <cesaandrade@Cesas-MacBook-Air.local>
2026-05-14 21:34:52 -0300

    Fix: convert frontend submodule to a regular folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,80 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import "./App.css";
+import { Toaster } from "./components/refine-ui/notification/toaster";
+import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import { dataProvider } from "./providers/data";
+import Dashboard from "./pages/Dashboard";
+import { BookOpen, Home, HomeIcon } from "lucide-react";
+import { Layout } from "./components/refine-ui/layout/layout";
+import SubjectList from "./pages/Subjects/SubjectList";
+import SubjectCreate from "./pages/Subjects/SubjectCreate";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <ThemeProvider>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              notificationProvider={useNotificationProvider()}
+              routerProvider={routerProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "XipgNI-DkJr1I-Cj2yzT",
+              }}
+              resources={[
+                {
+                name: "dashboard",
+                list: "/",
+                meta: {
+                  label : "Home",
+                  icon: <Home />
+                 } 
+                },
+                {
+                name: "subjects",
+                list: "/subjects",
+                create : "/subjects/create",
+                meta: {
+                  label : "Subjects",
+                  icon: <BookOpen />
+                 } 
+                }
+            ]}
+            >
+              {/* Stopped at 02:14:48 of the source in YT*/}
+              <Routes>
+                <Route element={<Layout>
+                  <Outlet/>
+                </Layout>}>
+                  <Route index element={<Dashboard />} />
+                  <Route path="subjects">
+                    <Route index element={<SubjectList/>}/>
+                    <Route path="create" element={<SubjectCreate/>}/>
+                  </Route>
+                </Route>
+              </Routes>
+              <Toaster />
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+          </DevtoolsProvider>
+        </ThemeProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
